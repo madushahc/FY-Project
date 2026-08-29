73f3856f75484efa90c3067055965e2e39f39b7d
madushahc <madushahc@gmail.com>
2026-08-29 11:17:25 +0530

    add

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -8,7 +8,7 @@ import { sendResetEmail } from "../utils/emailService.js";
 
 const generateToken = (id: string) => {
   return jwt.sign({ id }, process.env.JWT_SECRET || "secret", {
-    expiresIn: "30d",
+    expiresIn: "2h",
   });
 };
 
